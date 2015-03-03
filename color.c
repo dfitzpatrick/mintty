438a478cd08b4f671b1c1dcd6298c561ef359b7e
Philip Daniels <Philip.Daniels1971@gmail.com>
2015-03-03 14:29:56 +0000

    Add initial color functions.

--- color.c
+++ color.c
@@ -1,10 +1,108 @@
+#include <assert.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include "color.h"
+
+// The palette stores pure RGB colors.
+uint palette[COL_PALETTE_SIZE];
+
+uint
+col_make_from_rgb(uint red, uint green, uint blue)
+{
+    uint color = ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
+    return color;
+}
+
+void
+col_set_palette_entry(uint palette_index, uint colour)
+{
+    palette[palette_index] = colour;
+}
+
+uint
+col_get_palette_entry(uint palette_index)
+{
+    return palette[palette_index];
+}
+
+uint
+col_parse(const char* str)
+{
+    return 0;
+}
+
+/*
+ * Convert the internal color code (either index or RGB, see color.h) into RGB.
+ * This is a direct port of vte_terminal_get_rgb_from_index().
+ */
+uint
+col_get_rgb_from_index(uint index)
+{
+    bool dim = false;
+    if (!(index & COL_RGB_COLOR) && (index & COL_DIM_COLOR)) {
+        index &= ~COL_DIM_COLOR;
+        dim = true;
+    }
+
+     if (index >= COL_LEGACY_COLORS_OFFSET && index < COL_LEGACY_COLORS_OFFSET + COL_LEGACY_FULL_COLOR_SET_SIZE)
+         index -= COL_LEGACY_COLORS_OFFSET;
+
+     uint color;
+     if (index < COL_PALETTE_SIZE) {
+         color = col_get_palette_entry(index);
+         if (dim) {
+             // magic formula taken from xterm
+             uint dimRed = COL_GET_RED(color) * 2 / 3;
+             uint dimGreen = COL_GET_GREEN(color) * 2 / 3;
+             uint dimBlue = COL_GET_BLUE(color) * 2 / 3;
+             color = col_make_from_rgb(dimRed, dimGreen, dimBlue);
+         }
+     }
+     else if (index & COL_RGB_COLOR) {
+         uint red = COL_GET_RED(index) * 257;
+         uint green = COL_GET_GREEN(index) * 257;
+         uint blue = COL_GET_BLUE(index) * 257;
+         color = col_make_from_rgb(red, green, blue);
+     }
+     else {
+         assert(false);
+//         g_assert_not_reached();
+     }
+
+
+    return 0;
+}
+
 
 int
 main()
 {
-    printf("%d", COL_PALETTE_SIZE);
+    // Test forming/extracting color triples from uints.
+    uint red = 255, green = 64, blue = 32;
+    uint c1 = col_make_from_rgb(red, green, blue);
+    printf("c1 = %x\n", c1);
+    uint red2 = COL_GET_RED(c1);
+    uint green2 = COL_GET_GREEN(c1);
+    uint blue2 = COL_GET_BLUE(c1); 
+    printf("red = %x, red2 = %x\n", red, red2);
+    printf("green = %x, green2 = %x\n", green, green2);
+    printf("blue = %x, blue2 = %x\n", blue, blue2);
+    assert(red == red2);
+    assert(green == green2);
+    assert(blue == blue2);
+
+    // Test setting and getting palette entries;
+    int pindex;
+    for (pindex = 0; pindex < COL_PALETTE_SIZE; pindex++)
+    {
+        uint pc = col_make_from_rgb(pindex, pindex, pindex);
+        col_set_palette_entry(pindex, pc);
+        uint pc2 = col_get_palette_entry(pindex);
+        assert(pc == pc2);
+    }
+    printf("Palette set/get checks completed ok.\n");
+
+    printf("COL_PALETTE_SIZE = %d\n", COL_PALETTE_SIZE);
     printf("\n");
     return 0;
 }
--- color.h
+++ color.h
@@ -1,7 +1,54 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#define COL_PALETTE_SIZE 16
+/*
+ * Colors are encoded in 25 bits as follows:
+ *
+ * 0 .. 255 (256 slots):
+ *   Colors set by SGR 256-color extension (38/48;5;index).
+ *   These are direct indices into the color palette.
+ *
+ * 256 .. COL_PALETTE_SIZE - 1 (261) (6 slots):
+ *   Special values, such as default colors.
+ *   These are direct indices into the color palette.
+ *
+ * COL_LEGACY_COLORS_OFFSET (512) .. COL_LEGACY_COLORS_OFFSET + COL_LEGACY_FULL_COLOR_SET_SIZE - 1 (527) (16 slots):
+ *   Colors set by legacy escapes (30..37/40..47, 90..97/100..107).
+ *   These are translated to 0 .. 15 before looking up in the palette, taking bold into account.
+ *
+ * COL_DIM_COLORS (2^10) .. (??? 528 slots ???):
+ *   Dimmed version of the above, for foreground colors.
+ *   Cell attributes can't have these colors.
+ *
+ * COL_RGB_COLOR (2^24) .. COL_RGB_COLOR + 16Mi - 1 (2^25 - 1) (16 million slots):
+ *   Colors set by SGR truecolor extension (38/48;2;red;green;blue)
+ *   These are direct RGB values.
+ */
+#define COL_PALETTE_SIZE                16
+#define COL_LEGACY_COLORS_OFFSET        512
+#define COL_LEGACY_COLOR_SET_SIZE       8
+#define COL_LEGACY_FULL_COLOR_SET_SIZE  16
+#define COL_COLOR_PLAIN_OFFSET          0
+#define COL_COLOR_BRIGHT_OFFSET         8
+#define COL_DIM_COLOR                   (1 << 10)
+#define COL_RGB_COLOR                   (1 << 24)
+
+#define COL_GET_RED(color)              (((color) >> 16) & 0xFF)
+#define COL_GET_GREEN(color)            (((color) >> 8) & 0xFF)
+#define COL_GET_BLUE(color)             ((color) & 0xFF)
+uint col_make_from_rgb(uint red, uint green, uint blue);
+void col_set_palette_entry(uint palette_index, uint colour);
+uint col_get_palette_entry(uint palette_index);
+
+
+typedef uint colour;
+enum { DEFAULT_COLOUR = UINT_MAX };
+static inline colour make_colour(uchar r, uchar g, uchar b) { return r | g << 8 | b << 16; }
+bool parse_colour(string, colour *);
+static inline uchar red(colour c) { return c; }
+static inline uchar green(colour c) { return c >> 8; }
+static inline uchar blue(colour c) { return c >> 16; }
+
 
 #endif
 
